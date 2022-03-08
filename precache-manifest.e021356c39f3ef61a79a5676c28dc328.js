73975b9dc8dfb54d7c580e55a879778e9123608e
sasaLi <850852006@qq.com>
2022-03-08 20:33:52 +0800

    update

---
+++ precache-manifest.e021356c39f3ef61a79a5676c28dc328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03059afd8c63d0923e35",
+    "url": "/morney-test-website/css/app.538733c3.css"
+  },
+  {
+    "revision": "3fe30bdbe4a471f78792c58a86a0ab9a",
+    "url": "/morney-test-website/index.html"
+  },
+  {
+    "revision": "03059afd8c63d0923e35",
+    "url": "/morney-test-website/js/app.0d94bec5.js"
+  },
+  {
+    "revision": "e1a5d343db2b8f8a5658",
+    "url": "/morney-test-website/js/chunk-vendors.1435e2ed.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-test-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-test-website/robots.txt"
+  }
+]);
